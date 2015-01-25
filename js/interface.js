5cfab5629b3ffceca5658742ac51488fdecb0792
stepholdcorn <stephieoldcorn@gmail.com>
2015-01-25 19:26:26 +0000

    Adds the total running score

--- js/interface.js
+++ js/interface.js
@@ -1,17 +1,25 @@
 $(function() {
 
   // SETUP
-  var scorecard = new Scorecard ();
+  var scorecard = new Scorecard();
   var $newThrowForm = $('#newThrowForm');
+  var $totalScoreDisplay = $('#totalScoreDisplay');
   var $textInput = $('input:text');
 
   // ADD THROW SCORE
   $newThrowForm.on('submit', function(e) {
     e.preventDefault();
     var newNumber = $textInput.val();
-    scorecard.enterPins(newNumber);
+    var x = parseInt(newNumber, 10);
+    scorecard.enterPins(x);
     $('li:last').after('<li>' + newNumber + '</li>');
     $textInput.val('');
+  });
+
+  // SHOW TOTAL SCORE
+  $totalScoreDisplay.on('click', function() {
+    scorecard.totalCalculator();
+    $('h2').text(scorecard.totalScore);
   });
 
 });
